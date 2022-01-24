56e7b21443487ddb50a0e958e85ea39aba33624a
gaspurr <luik.gaspar@gmail.com>
2022-01-24 11:40:25 +0200

    Port change

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ const farmRoutes = require("./routes/farms")
 const bodyParser = require("body-parser")
 const path = require("path")
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 const app = express()
 app.use(cors())
 app.use(express.json())
