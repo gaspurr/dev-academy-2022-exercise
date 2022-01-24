5ed5d6782cc6e294d8030b0484c031ac918c1fe9
gaspurr <luik.gaspar@gmail.com>
2022-01-24 13:29:45 +0200

    package json file fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -40,9 +40,11 @@ mongoose
 
 // Step 1:
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.resolve(__dirname, "../../frontend/build")));
-    // Step 2:
-    app.get("*", function (req, res) {
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-    });
+    // Serve static files from the React frontend app
+    app.use(express.static(path.join(__dirname, '../../frontend/build')))
+
+    // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+    })
 }
